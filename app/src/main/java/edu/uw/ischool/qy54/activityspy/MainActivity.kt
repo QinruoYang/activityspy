2bdac81a185016dc9ffab001928180bd8a5bf67b
QinruoYang <qy54@uw.edu>
2023-10-18 18:40:22 -0700

    finish hw

--- app/src/main/java/edu/uw/ischool/qy54/activityspy/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/qy54/activityspy/MainActivity.kt
@@ -1,11 +1,40 @@
 package edu.uw.ischool.qy54.activityspy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate event fired - Bundle: $savedInstanceState")
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy event fired")
+        Log.e(TAG, "'You're going to need a bigger boat.' --Brody, Jaws")
     }
 }
